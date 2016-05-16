0b1a5c51646b03eb99541ca1a7e0b50291d90454
dbacinski <darek@macoscope.net>
2016-05-16 11:27:41 +0200

    Fixed meal count download

--- app/src/main/java/com/macoscope/ketchuplunch/model/MealService.kt
+++ app/src/main/java/com/macoscope/ketchuplunch/model/MealService.kt
@@ -7,8 +7,8 @@ class MealService(val scriptClient: ScriptClient) {
 
     fun getUserMeals(): List<Meal> {
         return scriptClient.getDataFromApi<List<String>>("mealNames")
-                .zip(scriptClient.getDataFromApi<List<String>>("mealCount"), { name, count ->
-                    Meal(name, count.replace("","0").toInt(), 0)
+                .zip(scriptClient.getDataFromApi<List<BigDecimal>>("mealCount"), { name, count ->
+                    Meal(name, count.toInt(), 0)
                 })
                 .zip(scriptClient.getDataFromApi<List<BigDecimal>>("mealTotalCount"), { meal, totalCount ->
                     meal.totalCount = totalCount.toInt()
