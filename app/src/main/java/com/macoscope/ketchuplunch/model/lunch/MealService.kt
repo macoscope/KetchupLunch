27cc6712eb16a503fc2ee53515ab5afee101ef28
dbacinski <darek@macoscope.net>
2016-05-19 14:28:52 +0200

    Added filtering of empty elements

--- app/src/main/java/com/macoscope/ketchuplunch/model/lunch/MealService.kt
+++ app/src/main/java/com/macoscope/ketchuplunch/model/lunch/MealService.kt
@@ -9,13 +9,10 @@ class MealService(val scriptClient: ScriptClient, val userName: String) {
     fun getUserMeals(dayIndex: Int): List<Meal> {
 
         val parameters: List<Any> = listOf(userName.split("@").first(), dayIndex)
-        val mealList = scriptClient.getDataFromApi<List<Map<String, Any>>>("meal", parameters)
+        val mealRawList = scriptClient.getDataFromApi<List<Map<String, Any>>>("meal", parameters)
 
-        val meals = mealList
-//                .filter {
-//                    val name = it["name"] as String
-//                    name.isNotEmpty()
-//                }
+        val meals = mealRawList
+                .filter { (it["name"] as String).isNotEmpty() }
                 .map {
                     it ->
                     Meal(it["name"] as String, (it["count"] as BigDecimal).toInt(), (it["totalCount"] as BigDecimal).toInt())
