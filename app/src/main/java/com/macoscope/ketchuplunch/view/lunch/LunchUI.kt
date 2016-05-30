b6633bb23faabd0fc8fcc0371ef74b0e83d65897
dbacinski <darek@macoscope.net>
2016-05-30 08:42:19 +0200

    Anko 0.9-beta1

--- app/src/main/java/com/macoscope/ketchuplunch/view/lunch/LunchUI.kt
+++ app/src/main/java/com/macoscope/ketchuplunch/view/lunch/LunchUI.kt
@@ -1,16 +1,11 @@
 package com.macoscope.ketchuplunch.view.lunch
 
 import android.app.Activity
-import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
-import android.support.v4.content.ContextCompat
 import android.view.View
 import com.macoscope.ketchuplunch.R
 import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
 import org.jetbrains.anko.appcompat.v7.toolbar
-import org.jetbrains.anko.backgroundColor
 import org.jetbrains.anko.design.appBarLayout
 import org.jetbrains.anko.design.tabLayout
 import org.jetbrains.anko.support.v4.viewPager
@@ -19,29 +14,29 @@ import org.jetbrains.anko.verticalLayout
 class LunchUI : AnkoComponent<Activity> {
     override fun createView(ui: AnkoContext<Activity>): View {
         return with(ui) {
-            verticalLayout {
+            verticalLayout(R.style.AppTheme) {
                 id = R.id.lunch_appbar
 
                 appBarLayout {
-                    backgroundColor = ContextCompat.getColor(ctx, R.color.colorRed)
+                    //backgroundColor = ContextCompat.getColor(ctx, R.color.colorRed)
 
                     toolbar {
                         id = R.id.lunch_toolbar
-                        setNavigationIcon(R.drawable.ic_nav)
-                        setTitleTextColor(Color.WHITE)
-                        overflowIcon!!.colorFilter = PorterDuffColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY)
+//                        setNavigationIcon(R.drawable.ic_nav)
+//                        setTitleTextColor(Color.WHITE)
+//                        overflowIcon!!.colorFilter = PorterDuffColorFilter(Color.WHITE, PorterDuff.Mode.MULTIPLY)
                     }
 
                     tabLayout {
                         id = R.id.lunch_tabs
-                        setTabTextColors(Color.LTGRAY, Color.WHITE)
-                        setSelectedTabIndicatorColor(Color.WHITE)
+//                        setTabTextColors(Color.LTGRAY, Color.WHITE)
+//                        setSelectedTabIndicatorColor(Color.WHITE)
                     }
                 }
 
                 viewPager {
                     id = R.id.lunch_pager_container
-                    backgroundColor = Color.WHITE
+//                    backgroundColor = Color.WHITE
                 }
             }
         }
