bf3b3f7a52192d34a0ad3b04928755cbbf3e4f37
Tomasz Kulikowski <tomasz@macoscope.net>
2016-06-13 17:54:06 +0200

    Added DropDownView for weeks adapter

--- app/src/main/java/com/macoscope/ketchuplunch/view/lunch/WeekToolbarItemUI.kt
+++ app/src/main/java/com/macoscope/ketchuplunch/view/lunch/WeekToolbarItemUI.kt
@@ -1,5 +1,6 @@
 package com.macoscope.ketchuplunch.view.lunch
 
+import android.graphics.Color
 import android.text.TextUtils
 import android.view.Gravity
 import android.view.View
@@ -7,21 +8,20 @@ import android.view.ViewGroup
 import com.macoscope.ketchuplunch.R
 import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.dip
 import org.jetbrains.anko.singleLine
+import org.jetbrains.anko.textColor
 import org.jetbrains.anko.textView
 
-class WeekItemUI : AnkoComponent<ViewGroup> {
+class WeekToolbarItemUI : AnkoComponent<ViewGroup> {
     override fun createView(ui: AnkoContext<ViewGroup>): View {
         return with(ui) {
             textView {
-                id = R.id.week_name
+                id = R.id.week_toolbar_name
                 singleLine = true
                 ellipsize = TextUtils.TruncateAt.END
                 textSize = 18f
-                height = dip(40f)
-                gravity = Gravity.CENTER_VERTICAL
-                setPadding(dip(8f), 0, dip(8f), 0)
+                gravity = Gravity.RIGHT
+                textColor = Color.WHITE
             }
         }
     }
--- app/src/main/java/com/macoscope/ketchuplunch/view/lunch/WeeksAdapter.kt
+++ app/src/main/java/com/macoscope/ketchuplunch/view/lunch/WeeksAdapter.kt
@@ -29,6 +29,20 @@ class WeeksAdapter : BaseAdapter() {
         var viewHolder: WeekHolder
         var convertViewVar = convertView
         if (convertViewVar == null) {
+            convertViewVar = WeekToolbarItemUI().createView(AnkoContext.create(parent!!.context, parent))
+            viewHolder = WeekHolder(convertViewVar.findViewById(R.id.week_toolbar_name) as TextView)
+            convertViewVar.tag = viewHolder
+        } else {
+            viewHolder = convertViewVar.tag as WeekHolder
+        }
+        viewHolder.bind(getItem(position))
+        return convertViewVar
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        var viewHolder: WeekHolder
+        var convertViewVar = convertView
+        if (convertViewVar == null) {
             convertViewVar = WeekItemUI().createView(AnkoContext.create(parent!!.context, parent))
             viewHolder = WeekHolder(convertViewVar.findViewById(R.id.week_name) as TextView)
             convertViewVar.tag = viewHolder
