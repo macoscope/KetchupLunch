b2c0550e3d75080477e050ed7f5e4f342035b55f
dbacinski <darek@macoscope.net>
2016-06-14 10:03:31 +0200

    Login test with Page Object

---
+++ app/src/androidTest/groovy/com/macoscope/ketchuplunch/LoginSpec.groovy
@@ -0,0 +1,46 @@
+package com.macoscope.ketchuplunch
+
+import android.content.Intent
+import android.support.test.rule.ActivityTestRule
+import com.macoscope.ketchuplunch.view.login.LoginActivity
+import org.junit.Rule
+import spock.lang.Specification
+
+class LoginSpec extends Specification {
+
+    @Rule
+    ActivityTestRule<LoginActivity> loginActivityRule = new ActivityTestRule(LoginActivity, true, false)
+
+    LoginFeature loginFeature = new LoginFeature(loginActivityRule)
+
+    def setup() {
+        loginFeature.setup()
+    }
+
+    def cleanup() {
+        loginFeature.cleanup()
+    }
+
+    def "moves to lunch screen when permission is granted and account is chosen by the user"() {
+        given:
+            def account = "darek@macoscope.net"
+        when:
+            loginActivityRule.launchActivity(new Intent(Intent.ACTION_MAIN))
+
+            LunchFeature lunchFeature = loginFeature.dismissPermissionRationale()
+                    .acceptAccountPermission()
+                    .chooseAccount(account)
+        then:
+            lunchFeature.assertToolbarIsDisplayed()
+    }
+
+    def "skips login screen when permission is granted and account is chosen by the user"() {
+        given:
+            loginFeature.setAccountPermissionGranted(true)
+            loginFeature.setChosenAccount("darek@macoscope.net")
+        when:
+            LunchFeature lunchFeature = loginFeature.launchActivity()
+        then:
+            lunchFeature.assertToolbarIsDisplayed()
+    }
+}
